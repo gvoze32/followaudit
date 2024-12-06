473ec4fd0bf6b186f4b5d795ed58a72e2cd3d154
gvoze32 <gvoze32@gmail.com>
2024-12-06 09:41:48 +0700

    Remove unused "Contact" link from FollowAudit component

--- src/components/logic/FollowAudit.tsx
+++ src/components/logic/FollowAudit.tsx
@@ -165,7 +165,6 @@ const FollowAudit: React.FC = () => {
             <div className="flex space-x-6 mt-4 md:mt-0">
               <a href="#" className="text-gray-400 hover:text-white transition-colors">Terms</a>
               <a href="#" className="text-gray-400 hover:text-white transition-colors">Privacy</a>
-              <a href="#" className="text-gray-400 hover:text-white transition-colors">Contact</a>
             </div>
           </div>
         </div>
